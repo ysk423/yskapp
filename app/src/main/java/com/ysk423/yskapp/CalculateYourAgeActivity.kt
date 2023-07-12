257bbb35b3fedec654c2141762dc0e086ee04a07
ysk423 <y_ino_0423@yahoo.co.jp>
2023-07-12 23:13:26 +0900

    add new Activity CalculateYourAgeActivity

---
+++ app/src/main/java/com/ysk423/yskapp/CalculateYourAgeActivity.kt
@@ -0,0 +1,11 @@
+package com.ysk423.yskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculateYourAgeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_your_age)
+    }
+}
